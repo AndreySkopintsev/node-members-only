a04e946ebf993b2e0f6ec1fadb801248b1e738c4
Andrey Skopintsev <sanchezzvilapandozz@gmail.com>
2020-08-17 12:02:53 +0300

    added POST request to sign up page

--- routes/members.js
+++ routes/members.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const membersRouter = express.Router()
+const User = require('../models/userModel')
+const bcrypt = require('bcrypt')
 
 //Get home page
 membersRouter.get('/home',(req,res)=>{
@@ -10,6 +12,25 @@ membersRouter.get('/home',(req,res)=>{
 //Get request for sign up page
 membersRouter.get('/signup',(req,res)=>{
     res.render('sign_up',{title:'Sign Up'})
+})
+
+//POST request for sign up page
+membersRouter.post('/signup',(req,res)=>{
+    bcrypt.hash(req.body.password,10,(err,hashedPassword)=>{
+        if(err){return next(err)}
+        const user = new User({
+            firstName:req.body.firstname,
+            lastName:req.body.lastname,
+            username:req.body.username,
+            password:hashedPassword,
+            status:true
+        })
+        user.save(err => {
+            if(err){return next(err)}
+
+        })
+        res.redirect('/membersOnly/home')
+    })
 })
 
 module.exports = membersRouter
